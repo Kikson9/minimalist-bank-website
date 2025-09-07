552691b20b3b1fdc8d0aa6e6551d01ed137c36d2
Kikson9 <tochukwuokike7@gmail.com>
2025-09-07 10:52:21 +0800

    Reveal sections smoothly on scroll

--- script.js
+++ script.js
@@ -110,3 +110,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections on scroll
+const revealSection = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+const allSections = document.querySelectorAll('.section');
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
